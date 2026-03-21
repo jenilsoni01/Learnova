f16630e06a0739ea026edf04f8af62734d077d46
Jenil Soni <jenilns369@gmail.com>
2026-03-21 10:34:17 +0530

    initial commit for backend

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv" //(module)
+import connectDB from "./db/index.js"
+import app from "./app.js"
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB() // connecting db
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {// serever init.
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => { // error handling
+    console.log("MONGO db connection failed !!! ", err); 
+})
+
+
